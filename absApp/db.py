44f734c8de8a9af8bc0d45876f3a1610b2935620
This-is-UserNamee <so1031eg@gmail.com>
2024-09-11 14:29:44 +0900

    Add files via upload

--- absApp/db.py
+++ absApp/db.py
@@ -2,13 +2,6 @@ import sqlite3
 
 DATABASE = 'database.db'
 
-def create_books_table():
-    con = sqlite3.connect(DATABASE)
-    con.execute("CREATE TABLE IF NOT EXISTS books (month, reason, name)")
-    con.close()import sqlite3
-
-DATABASE = 'database.db'
-
 def create_months_table():
     con = sqlite3.connect(DATABASE)
     con.execute("CREATE TABLE IF NOT EXISTS month1 (name, reason)")
