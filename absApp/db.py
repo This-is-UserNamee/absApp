e1cb98d33f54d0f0c7b48e54b2aa92450ec2cafd
kiiiiiiiiiiiiiiii <kiichi20040428@gmail.com>
2024-09-11 19:55:43 +0900

    フォーム入力4

--- absApp/db.py
+++ absApp/db.py
@@ -3,17 +3,10 @@ import sqlite3
 DATABASE = 'database.db'
 
 def create_months_table():
+    i = 0
     con = sqlite3.connect(DATABASE)
-    con.execute("CREATE TABLE IF NOT EXISTS month1 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month2 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month3 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month4 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month5 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month6 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month7 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month8 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month9 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month10 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month11 (name, reason)")
-    con.execute("CREATE TABLE IF NOT EXISTS month12 (name, reason)")
+    while i < 12:
+        i2 = i+1
+        con.execute("CREATE TABLE IF NOT EXISTS month"+str(i2)+" (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,reason TEXT)")
+        i = i + 1
     con.close()
--- absApp/main.py
+++ absApp/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect, url_for
 import db
 import sqlite3
+import datetime
 
 DATABASE = 'database.db'
 
@@ -11,44 +12,28 @@ db.create_months_table()
 
 @app.route('/')
 def index():
-    
-    monthN = request.args.get('monthN')
-    
+    current_month = datetime.datetime.now().month
+    default_monthN = 'month'+str(current_month)
+    monthN = request.args.get('monthN', default_monthN)
     con = sqlite3.connect(DATABASE)
-    if monthN == 'month1':
-        db_month = con.execute('SELECT * FROM month1').fetchall()
-    elif monthN == 'month2':
-        db_month = con.execute('SELECT * FROM month2').fetchall()
-    elif monthN == 'month3':
-        db_month = con.execute('SELECT * FROM month3').fetchall()
-    elif monthN == 'month4':
-        db_month = con.execute('SELECT * FROM month4').fetchall()
-    elif monthN == 'month5':
-        db_month = con.execute('SELECT * FROM month5').fetchall()
-    elif monthN == 'month6':
-        db_month = con.execute('SELECT * FROM month6').fetchall()
-    elif monthN == 'month7':
-        db_month = con.execute('SELECT * FROM month7').fetchall()
-    elif monthN == 'month8':
-        db_month = con.execute('SELECT * FROM month8').fetchall()
-    elif monthN == 'month9':
-        db_month = con.execute('SELECT * FROM month9').fetchall()
-    elif monthN == 'month10':
-        db_month = con.execute('SELECT * FROM month10').fetchall()
-    elif monthN == 'month11':
-        db_month = con.execute('SELECT * FROM month11').fetchall()
-    else:
-        db_month = con.execute('SELECT * FROM month12').fetchall()
+    db_month = con.execute('SELECT * FROM '+monthN).fetchall()
     con.close()
     
-    
-    month = []
-    for row in db_month:
-        month.append({'name': row[0], 'reason': row[1]})
+    months = [[]]
+    i = 1
+    while i < 13:
+        con = sqlite3.connect(DATABASE)
+        db_month = con.execute('SELECT * FROM month'+str(i)).fetchall()
+        con.close()
+        month = []
+        for row in db_month:
+            month.append({'id': row[0], 'name': row[1], 'reason': row[2]})
+        months.append(month)
+        i = i + 1
         
     return render_template(
         'index.html',
-        month=month
+        months=months
     )
 
 @app.route('/form')
@@ -61,37 +46,25 @@ def form():
 def register():
     reason = request.form['reason']
     name = request.form['name']
-    month = request.form['month']
-    
+    month = str(request.form['month'])
+    sql = 'INSERT INTO month'+month+' (name, reason) VALUES (?, ?)'
     con = sqlite3.connect(DATABASE)
-    if month == '1':
-        con.execute('INSERT INTO month1 VALUES(?, ?)', [name, reason])
-    elif month == '2':
-        con.execute('INSERT INTO month2 VALUES(?, ?)', [name, reason])
-    elif month == '3':
-        con.execute('INSERT INTO month3 VALUES(?, ?)', [name, reason])
-    elif month == '4':
-        con.execute('INSERT INTO month4 VALUES(?, ?)', [name, reason])
-    elif month == '5':
-        con.execute('INSERT INTO month5 VALUES(?, ?)', [name, reason])
-    elif month == '6':
-        con.execute('INSERT INTO month6 VALUES(?, ?)', [name, reason])
-    elif month == '7':
-        con.execute('INSERT INTO month7 VALUES(?, ?)', [name, reason])
-    elif month == '8':
-        con.execute('INSERT INTO month8 VALUES(?, ?)', [name, reason])
-    elif month == '9':
-        con.execute('INSERT INTO month9 VALUES(?, ?)', [name, reason])
-    elif month == '10':
-        con.execute('INSERT INTO month10 VALUES(?, ?)', [name, reason])
-    elif month == '11':
-        con.execute('INSERT INTO month11 VALUES(?, ?)', [name, reason])
-    else:
-        con.execute('INSERT INTO month12 VALUES(?, ?)', [name, reason])
+    con.execute(sql, (name, reason))
     con.commit()
     con.close()
     return redirect(url_for('index', monthN = 'month'+month))
     
+@app.route('/delete')
+def delete():
+    id = request.args.get('id')
+    monthN = request.args.get('monthN')
+    print(id)
+    print(monthN)
+    # con = sqlite3.connect(DATABASE)
+    # con.execute('DELETE FROM '+monthN+' WHERE name="apple"')
+    # con.commit()
+    # con.close()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
